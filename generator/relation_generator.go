cff8a3824efde0ea3f7ff04b1c5bc1275ea2434c
darwishdev <a.darwish.dev@gmail.com>
2025-10-30 23:12:40 +0200

    Polish: Accept Temprature

--- generator/relation_generator.go
+++ generator/relation_generator.go
@@ -12,6 +12,7 @@ import (
 type RelationGenerator[T any] struct {
 	RelationEntity      string
 	RelationContext     string
+	Temperature         float32
 	RelationRecordJSON  string
 	Instructions        string
 	Examples            []genaistructbuilder.RelationExample[T]
@@ -24,7 +25,7 @@ func (g *RelationGenerator[T]) Execute(ctx context.Context, generateContent gena
 	if err != nil {
 		return err
 	}
-	config := internal.GenerateConfig(ctx, g.Instructions, schema)
+	config := internal.GenerateConfig(ctx, g.Instructions, schema, g.Temperature)
 	mainPrompt := fmt.Sprintf(
 		"Task: Generate a %s record based on the provided input JSON.\nContext: %s\nInput JSON: %s",
 		g.RelationEntity,
